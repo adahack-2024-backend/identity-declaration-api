a8cdc109b673bbb4b7d2ac2967414a7fd2e18ac7
RudsonSantana <rudsonsanttana@gmail.com>
2024-04-12 20:57:27 -0300

    feature/struct

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,42 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
+import { routes } from './routes';
 import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors'
+import winston from 'winston';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-const app = express();
-app.use(helmet());
+; (async () => {
+  const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env'
+  dotenv.config({ path: envFile })
 
-app.use(express.json());
+  const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format((info) => {
+        info.timestamp = format(new Date(), 'dd/MM/yyyy HH:mm:ss', { locale: ptBR });
+        return info;
+      })(),
+      winston.format.simple(),
+    ),
+    transports: [new winston.transports.Console()]
+  })
 
+  const app = express();
+  app.use(morgan("dev", { stream: { write: message => logger.info(message.trim()) } }));
+  app.use(express.json());
+  app.use(helmet())
+  app.use(cors())
+  app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+  app.use(routes);
+
+  const port = process.env.PORT
+
+  app.listen(port, () => {
+    logger.info(`Server started!`);
+  });
+})()
