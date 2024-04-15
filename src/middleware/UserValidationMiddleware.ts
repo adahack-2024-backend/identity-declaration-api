4bfdeb2056cda53f2b66cfa21c3670968cc86e16
RudsonSantana <rudsonsanttana@gmail.com>
2024-04-15 15:57:25 -0300

    refactor: UserValidationMiddleware

--- src/middleware/UserValidationMiddleware.ts
+++ src/middleware/UserValidationMiddleware.ts
@@ -1,18 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
+import { body } from 'express-validator';
 
-export function validateUserData(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(400).json({ message: 'Email and password are required.' });
+export async function validateUserData(req: Request, res: Response, next: NextFunction) {
+    try {
+        const emailInvalid = await body('email').notEmpty().isEmail().withMessage('email not valid').run(req);
+        const passwordInvalid = await body('password').isStrongPassword({
+            minLength: 6, 
+            minLowercase: 1, 
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        }).run(req);
+
+        if (!emailInvalid.isEmpty()) return res.status(400).send({ error: 'email not valid' });
+        if (!passwordInvalid.isEmpty()) return res.status(400).send({ error: 'password not valid' });
+
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+        next(error);
     }
-
-    if (!/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email)) {
-        return res.status(400).json({ message: 'Invalid email format.' });
-    }
-
-    if (password.length < 8) {
-        return res.status(400).json({ message: 'Password must be at least 8 characters long.' });
-    }
-
-    next();
 }
+
+
+
+
